a06d0df0b943071088460cc30f53ced938ca6d42
Salma Noe <sattari2101@gmail.com>
2021-02-24 11:05:36 -0800

    factorial

--- src/recursion.js
+++ src/recursion.js
@@ -6,7 +6,16 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
+var result = 1;
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  result = n * factorial(n - 1);
+  return result;
 };
 
 // 2. Compute the sum of an array of integers.
